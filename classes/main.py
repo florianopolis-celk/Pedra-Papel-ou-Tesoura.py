dea4ccd85422250e5fba7e3c216199f56b4f3a26
pelucasdev <casadoeinstein@gmail.com>
2022-12-16 07:33:55 -0300

    classes

---
+++ classes/main.py
@@ -0,0 +1,15 @@
+class Conta:
+    #funções criadas dentro de classes 
+    #saõ chamados de métodos
+    def calcular(self):
+        pass
+
+item1 = Conta()
+item1.nome = 'joao richneirders'
+item1.cor_carro = 'vermelho'
+item1.placa = 123
+
+#print(type(item1))
+print(type(item1.nome))
+print(type(item1.cor_carro))
+print(type(item1.placa))
